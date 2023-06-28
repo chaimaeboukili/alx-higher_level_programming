0fc5750a5171ef4a0d900cf3b5678b3c653e19d3
chaimaeboukili <cboukili@hotmail.fr>
2023-06-28 09:22:59 -0700

    mandatory

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
         Args:
         size (int): The size of the new square.
         """
-        if type(size) is not int:
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
