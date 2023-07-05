fe43c63f7b0157b40e8857b89289c5413c597ac8
chaimaeboukili <cboukili@hotmail.fr>
2023-07-05 14:56:00 -0700

    fix

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-# 101-locked_class.py
+"""Module containing a class with locked down attributes"""
+
 class LockedClass:
-     __slots__ = ["first_name"]
+    """Class with locked down attributes. Can only add first_name"""
+
+    def __setattr__(self, name, value):
+        if name != "first_name":
+            raise AttributeError("'LockedClass' object has no attribute 'last_name'")
+        else:
+            self.__dict__[name] = value
